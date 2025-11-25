4c3e27e405d7101b23696664e3bc6ec3891c9743
Jinyu Hu <jinyu.hu@student.kuleuven.be>
2025-11-25 16:11:40 +0100

    merge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { initAnalytics } from './lib/analytics'
+import { Toaster } from "sonner"
 
 // Initialize Google Analytics
 initAnalytics();
-import { Toaster } from "sonner"
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
