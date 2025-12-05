ee70cbc10bf98a8aecbd8d0f5585c0fd2a147de0
Simeon <61100437+simeon2002@users.noreply.github.com>
2025-12-05 15:13:03 +0100

    Merge fix: Fix dashboard page not showing after merge:
    - add missing import
    - added unknown error catch blocks
    - fixed goal.exercisesDiscovered to match backend data type goal.exercisesCompleted
    - fixed global tyep error => added tidiochatapi to vite-env.d.ts to remove errors.

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,17 +1,27 @@
 ﻿declare module "*.svg" {
-    import * as React from "react";
-    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
+  import * as React from "react";
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+  const src: string;
+  export default src;
 }
 
 declare module "*.svg?react" {
-    import * as React from "react";
-    const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-    export default ReactComponent;
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
 }
 
+declare module "*.png" {
+  const value: string;
+}
 
-declare module '*.png' {
-    const value: string;
+interface Window {
+  tidioChatApi: {
+    show: () => void;
+    hide: () => void;
+    open: () => void;
+    close: () => void;
+  };
 }
