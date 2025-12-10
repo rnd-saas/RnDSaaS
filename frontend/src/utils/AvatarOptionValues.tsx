6bf88caa2b70588458d32d36e4662e6a02220b35
Aikatya <katya.gatova@yandex.com>
2025-12-10 23:02:53 +0100

    replaced placeholder avatars

--- frontend/src/utils/AvatarOptionValues.tsx
+++ frontend/src/utils/AvatarOptionValues.tsx
@@ -1,11 +1,15 @@
-import avatar2 from "@/assets/avatars/sarah_avatar.png";
-import avatar1 from "@/assets/avatars/tom_avatar.png";
+import avatar0 from "@/assets/avatars/avatar0.png";
+import avatar1 from "@/assets/avatars/avatar1.png";
+import avatar2 from "@/assets/avatars/avatar2.png";
+import avatar3 from "@/assets/avatars/avatar3.png";
+import avatar4 from "@/assets/avatars/avatar4.png";
+import avatar5 from "@/assets/avatars/avatar5.png";
 
 export const AvatarOptionValues=[
-    {fallback:"option0", src:avatar2, value:0},
+    {fallback:"option0", src:avatar0, value:0},
     {fallback:"option1", src:avatar1, value:1},
     {fallback:"option2", src:avatar2, value:2},
-    {fallback:"option3", src:avatar1, value:3},
-    {fallback:"option4", src:avatar2, value:4},
-    {fallback:"option5", src:avatar1, value:5},
+    {fallback:"option3", src:avatar3, value:3},
+    {fallback:"option4", src:avatar4, value:4},
+    {fallback:"option5", src:avatar5, value:5},
 ]
